0fd3c07363f86f7f95674281abbe0d370063efe4
colorlessenergy <brianmunozdev@gmail.com>
2019-05-26 05:35:50 -0700

    create rooms component, add route to rooms component, and add link to rooms component

--- inventory-system-frontend/src/App.js
+++ inventory-system-frontend/src/App.js
@@ -3,6 +3,7 @@ import {Route, HashRouter, Link} from 'react-router-dom';
 
 import Login from './containers/Login/Login';
 import Register from './containers/Register/Register';
+import Rooms from './containers/Rooms/Rooms';
 
 class App extends Component {
   render () {
@@ -12,7 +13,9 @@ class App extends Component {
           <p>hello</p>
           <Link to="/login">Login</Link>
           <Link to="/register">register</Link>
+          <Link to="/rooms">rooms</Link>
           <Route path='/login' exact component={Login} />
+          <Route path='/rooms' exact component={Rooms} />
           <Route path='/register' exact component={Register} />
         </div>
       </HashRouter>
