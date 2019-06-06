479fb032ebf676192c183c1434aeb3ce2fded9e5
Daniel Munoz <daniel.munozdev@gmail.com>
2019-06-06 15:58:10 -0700

    add requested changes as per #56

--- inventory-system-frontend/src/Components/UI/Form/Form.js
+++ inventory-system-frontend/src/Components/UI/Form/Form.js
@@ -2,22 +2,23 @@ import React from 'react';
 
 /**
   form to handle user input
-  @prop {String} title - form title
-  @prop {Array} inputs - Array of objects with info for each form group (label and input)
-    @prop {String} inputs[i].key - unique key for each form group
-    @prop {String} inputs[i].for - label for and input id value
-    @prop {String} inputs[i].label - label name
-    @prop {String} inputs[i].type - input type
+  @prop {String} formTitle - form title
+  @prop {Array} inputsData - Array of objects with info for each form group (label and input)
+    @prop {String} inputsData[i].key - unique key for each form group
+    @prop {String} inputsData[i].for - label for and input id value
+    @prop {String} inputsData[i].labelText - label text
+    @prop {String} inputsData[i].type - input type
   @prop {Function} onChange - event listener on input to sync form data with container state
   @prop {Function} onSubmit - event listener on form to send request to API
+  @prop {String} buttonText - submit button text
 */
 
 const Form = (props) => {
   console.log('props', props);
-  let inputList = props.inputs.map((input) => {
+  let inputList = props.inputsData.map((input) => {
     return (
       <div key={input.key}>
-        <label htmlFor={input.for}>{input.label}</label>
+        <label htmlFor={input.for}>{input.labelText}</label>
         <input id={input.for} type={input.type} onChange={props.onChange} />
       </div>
     );
@@ -27,9 +28,9 @@ const Form = (props) => {
   return (
     <React.Fragment>
       <form onSubmit={props.onSubmit}>
-        <h3>{props.title}</h3>
+        <h1>{props.formTitle}</h1>
         {inputList}
-        <button type="submit">Create New Item</button>
+        <button type="submit">{props.buttonText}</button>
       </form>
     </React.Fragment>
   );
