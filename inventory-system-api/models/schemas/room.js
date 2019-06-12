1e072dd0ba2504053a568c890f1cfed7fa3d7ff7
Daniel Munoz <daniel.munozdev@gmail.com>
2019-06-11 18:19:47 -0700

    add styles to room component as per #90 and update API for creating rooms

--- inventory-system-api/models/schemas/room.js
+++ inventory-system-api/models/schemas/room.js
@@ -7,12 +7,14 @@ let roomSchema = new Schema({
   code: String,
   creator: {type: Schema.Types.ObjectId, ref: 'User'},
   items: [{ type: Schema.Types.ObjectId, ref: 'Item' }],
-  name: String,
+  name: {type: String, required: true},
   users: [{ type: Schema.Types.ObjectId, ref: 'User' }],
 });
 
 roomSchema.pre('save', function (next) {
-
+  if (!this.name) {
+    return res.status(400).send('Missing room name');
+  }
   next();
 });
 
